938b74eb0eb4258c4ca05c0e3190ed82aea93333
Victor Hugo Souza <127329074+victorhbfsouza@users.noreply.github.com>
2023-09-19 16:19:38 -0300

    :zap: Update Cronometro

--- baltaIO/Cronometro/Program.cs
+++ baltaIO/Cronometro/Program.cs
@@ -1,28 +1,71 @@
-﻿
-Console.WriteLine("\n========= CRONOMETRO C# =========\n");
-int segundos = 0;
-Console.Write("Digite o tempo em segundos do cronometro: ");
-segundos = int.Parse(Console.ReadLine());
-Start(segundos);
+﻿using System;
+using System.Threading;
 
-
-static void Start(int time)
+namespace Stopwatch
 {
-    int currenTime = 0;
-
-    while(currenTime != time)
+    class Program
     {
-        Console.Clear();
-        Console.WriteLine("\n========= CRONOMETRO C# =========\n");
-        currenTime++;
-        Console.WriteLine(currenTime);
-        Thread.Sleep(1000);
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("\n================= CRONOMETRO =================\n");
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("\nQuanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Thread.Sleep(1000);
+            Console.WriteLine("Começando em 3");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Começando em 2");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Começando em 1");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            Start(time);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Cronometro finalizado, retornando ao menu inicial.");
+            Thread.Sleep(2500);
+            Menu();
+        }
     }
-
-    Console.Clear();
-    Console.WriteLine("\nCronometro finalizado!\n\nAperte a tecla ENTER e finalize o programa!");
-    Console.ReadLine();
-
-
 }
-
