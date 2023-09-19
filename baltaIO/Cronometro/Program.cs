ee20bd3d53e7dbdb243ba1498e0ac131412fac71
Victor Hugo Souza <127329074+victorhbfsouza@users.noreply.github.com>
2023-09-19 15:23:53 -0300

    :tada: Cronometro

---
+++ baltaIO/Cronometro/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.WriteLine("\n========= CRONOMETRO C# =========\n");
+int segundos = 0;
+Console.Write("Digite o tempo em segundos do cronometro: ");
+segundos = int.Parse(Console.ReadLine());
+Start(segundos);
+
+
+static void Start(int time)
+{
+    int currenTime = 0;
+
+    while(currenTime != time)
+    {
+        Console.Clear();
+        Console.WriteLine("\n========= CRONOMETRO C# =========\n");
+        currenTime++;
+        Console.WriteLine(currenTime);
+        Thread.Sleep(1000);
+    }
+
+    Console.Clear();
+    Console.WriteLine("\nCronometro finalizado!\n\nAperte a tecla ENTER e finalize o programa!");
+    Console.ReadLine();
+
+
+}
+
