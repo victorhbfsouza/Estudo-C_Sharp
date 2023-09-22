7e1b153f8ac04ae6d3f501a4f13e7c20a72b58cb
Victor Hugo Souza <127329074+victorhbfsouza@users.noreply.github.com>
2023-09-22 15:26:46 -0300

    :tada: Moedas

---
+++ baltaIO/Moedas/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Globalization;
+
+namespace Moedas;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+        #region Formatando Moedas
+
+        decimal valor = 10.25m;
+        Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));
+
+        decimal valorE = 10.25m;
+        Console.WriteLine(valorE.ToString("C", CultureInfo.CreateSpecificCulture("es-ES")));
+
+        decimal valorD = 10.25m;
+        Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+
+        //MAIS UTILIZADOS abaixo
+
+        decimal valorNumero = 100.257m;
+        Console.WriteLine(valorNumero.ToString("N", CultureInfo.CreateSpecificCulture("pt-BR")));
+
+        decimal valorPorcentagem = 100.257m;
+        Console.WriteLine(valorPorcentagem.ToString("P", CultureInfo.CreateSpecificCulture("pt-BR")));
+
+        decimal valorCMoeda = 10.25m;
+        Console.WriteLine(valorCMoeda.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));
+
+        Console.WriteLine("\n\n");
+        #endregion
+
+        #region Math
+
+        //Arredondar
+        decimal valorMath = 10536.25m;
+        Console.WriteLine(Math.Round(valorMath));
+        Console.WriteLine();
+
+        //Arredondar para cima
+        Console.WriteLine(Math.Ceiling(valorMath));
+        Console.WriteLine();
+
+        //Arredondar para baixo
+        Console.WriteLine(Math.Floor(valorMath));
+        Console.WriteLine();
+        Console.WriteLine();
+
+        #endregion
+
+
+
+        Console.ReadLine();
+
+    }
+}
