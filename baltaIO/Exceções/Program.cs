6dbe2e660db71a95a23b1bed1cbe491382aa04fe
Victor Hugo Souza <127329074+victorhbfsouza@users.noreply.github.com>
2023-09-22 16:48:01 -0300

    :tada: Exceções

---
+++ baltaIO/Exceções/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Exceções;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var array = new int[3];
+
+        try 
+        { 
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(array[i]);
+            //}
+            Salvar();
+
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.InnerException);
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Verifique o índice do array");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.InnerException);
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Ops, algo deu errado, tente novamente!");
+        }
+
+        Console.ReadLine();
+    }
+
+    public static void Cadastrar(string texto)
+    {
+        if (string.IsNullOrEmpty(texto))
+            throw new Exception("O texto nãpo pode ser nulo ou vazio.");
+    }
+}
